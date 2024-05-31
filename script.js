9bc8cff81b6b42fda3b3eb3805c8220f871e8ec4
Sakina Naqvi <82605626+sakinanaqvi@users.noreply.github.com>
2024-05-30 20:12:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('learn-more-btn').addEventListener('click', function() {
+    // You can add functionality to the button here, like showing/hiding additional information
+    alert('Functionality under construction');
+  });
