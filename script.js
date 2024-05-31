939d9043859e06f19121a5e3d433beafb3c9e8bd
Sakina Naqvi <82605626+sakinanaqvi@users.noreply.github.com>
2024-05-30 20:35:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('learn-more-btn').addEventListener('click', function() {
+/*document.getElementById('learn-more-btn').addEventListener('click', function() {
     // You can add functionality to the button here, like showing/hiding additional information
     alert('Functionality under construction');
-  });
+  });*/
